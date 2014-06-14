b0379bc5f1ebc2773443abb05075f8f58058084f
Kim Kong <kongqingzhang@gmail.com>
2014-06-14 15:14:49 +0800

    write_host_file

--- set_goagent.py
+++ set_goagent.py
@@ -3,13 +3,14 @@
 # auther Kim Kong  kongqingzhang@gmail.com
 import pdb
 import sys
-from GCC import ip_set, net_address_set
+from GCC import  net_address_set
 import threading
 import time
 import ConfigParser
 
 
-
+init_threading_count = threading.activeCount()
+ipList = []
 
 
 def get_host(ip_address):
@@ -32,19 +33,19 @@ def get_host(ip_address):
             nameList = str(e).split("', '")
             nameList[0] = nameList[0].split("'")[-1]
             nameList[-1] = nameList[-1].split("'")[0]
-            print ip_address
-            return nameList
+            # print ip_address + '\n'
+            return [nameList, ip_address]
         elif 'match' in str(e):
             tempList = str(e).split("'")
             nameList.append(tempList[-2])
-            print ip_address
-            return nameList
+            # print ip_address
+            return [nameList, ip_address]
         return e
     except Timeout:
         # print ip_address + ' time out'
         return -3
     except Exception as e:
-        print ip_address + '\n' + str(e)
+        # print ip_address + '\n' + str(e)
         return -4
 
 
@@ -57,35 +58,45 @@ def get_ip(ip_s):
     """
     read 3-tuplues, get host and filter to iter, it's some bugs because I don't know  thread well
     """
-    print ip_s
+    # print ip_s
     for ip_tuple in ip_s:
         for nu in range(ip_tuple[1], ip_tuple[2] + 1):
             ip = ip_tuple[0] + '.' + str(nu)
             get_thread = GetHost(ip)
-            tempList = get_thread.start()
-            if str(tempList).find('android.com') != -1:
-                yield {ip: 'android'}
-            if str(tempList).find('ggpht') != -1:
-                yield {ip: 'ggpht'}
-            if str(tempList).find('gstatic.com') != -1:
-                yield {ip: 'gstatic'}
-            if str(tempList).find('googleapis') != -1:
-                yield {ip: 'googleapis'}
-            if str(tempList).find('talk') != -1:
-                yield {ip: 'talk'}
-            if str(tempList).find('googleusercontent') != -1:
-                yield {ip: 'googleusercontent'}
-            if str(tempList).find('googlecode') != -1:
-                yield {ip: 'googlecode'}
-            if str(tempList).find('googlesource') != -1:
-                yield {ip: 'googlesource'}
-            if str(tempList).find('googlevideo') != -1:
-                yield {ip: 'googlevideo'}
-            if str(tempList).find('googlegroups') != -1:
-                yield {ip: 'googlegroups'}
-            if str(tempList).find('google') != -1:
-                yield {ip: 'google'}
+            get_thread.start()
+    print threading.activeCount() - init_threading_count, 'threading working...'
+    while threading.activeCount() > init_threading_count:
+        pass
 
+def filter_ip(ip_ss):
+    for tempList in ip_ss:
+        if str(tempList[0]).find('android.com') != -1:
+            yield {tempList[1]: 'android'}
+        if str(tempList[0]).find('ggpht') != -1:
+            yield {tempList[1]: 'ggpht'}
+        if str(tempList[0]).find('gstatic.com') != -1:
+            yield {tempList[1]: 'gstatic'}
+        if str(tempList[0]).find('googleapis') != -1:
+            yield {tempList[1]: 'googleapis'}
+        if str(tempList[0]).find('talk') != -1:
+            yield {tempList[1]: 'talk'}
+        if str(tempList[0]).find('googleusercontent') != -1:
+            yield {tempList[1]: 'googleusercontent'}
+        if str(tempList[0]).find('googlecode') != -1:
+            yield {tempList[1]: 'googlecode'}
+        if str(tempList[0]).find('googlesource') != -1:
+            yield {tempList[1]: 'googlesource'}
+        if str(tempList[0]).find('googlevideo') != -1:
+            yield {tempList[1]: 'googlevideo'}
+        if str(tempList[0]).find('googlegroups') != -1:
+            yield {tempList[1]: 'googlegroups'}
+        if str(tempList[0]).find('google') != -1:
+            yield {tempList[1]: 'google'}
+
+
+    # print threading.activeCount() - init_threading_count, 'threading working...'
+    # while threading.activeCount() > init_threading_count:
+    #     pass
 
 
 def dic_to_config(input_iter):
@@ -108,13 +119,13 @@ def dic_to_config(input_iter):
 
 def net_address(net_address_s):
     """
-    "192.169.1.0/24" net address change to tuplues ("192.168.1", 0, 255)
+    "192.168.1.0/24" net address change to tuplues ("192.168.1", 0, 255)
     """
     ipList = []
     def get_ip_number_list(m, ip_number):
         n = 0
         temp = 2 ** m
-        while True:
+        while 1:
             if m * n <= ip_number < m * (n + 1):
                 return [m * n, m * (n + 1)]
             n += 1
@@ -141,7 +152,8 @@ class GetHost(threading.Thread):
         global lock
         try:
             lock.acquire()
-            return a
+            if isinstance(a, list):
+                ipList.append(a)
         except:
             pass
         finally:
@@ -150,8 +162,8 @@ class GetHost(threading.Thread):
         pass
 
 def run_pro():
-    dic_to_config(get_ip(net_address(net_address_set)))
-
+    get_ip(net_address(net_address_set))
+    dic_to_config(filter_ip(ipList))
 
 
 
