248b63f068bab427bc4e0394903755e4bbf9d8a1
Kim Kong <kongqingzhang@gmail.com>
2014-06-14 11:45:06 +0800

    use requests

--- set_goagent.py
+++ set_goagent.py
@@ -1,112 +1,162 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-__author__ = 'mhohai'
-"""
-For GoAgent choice fast ip
-"""
-
-import re
+#coding:utf-8
+# auther Kim Kong  kongqingzhang@gmail.com
+import pdb
+import sys
+from GCC import ip_set, net_address_set
 import threading
-import subprocess
-
-#compile regex
-"""
-I'm using Ubuntu, re.compile by ping and w3m stdout.
-Other system should change it to compare your PC.
-please: $sudo apt-get install w3m
-"""
-re_avg_time = re.compile(r'\d+/(\d+)', re.M)
-re_loss_percent = re.compile(r'(\d+)%', re.M)
-# Exact match host name. old:([^:]+)
-re_find_dns_name = re.compile(r'dNSName=[^:]*?\b(google\.\w+\.?\w*)', re.M)
-
-#ip_set = [('0.0.0', min, max), ]
-#ip_set = [('203.208.46', 140, 145), ]
-from GCC import ip_set  # setting ip set first.
-#ip_str = "IP|IP" #Easy add yourself ip.
-ip_str = None
-
-ip_list = []
-init_threading_count = threading.activeCount()
+import time
+import ConfigParser
 
 
-def find_host(ip):
-    w3m_cmd_command = ('w3m', 'https://'+ip)
-    w3m_https = subprocess.Popen(w3m_cmd_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    w3m_https.wait()
-    w3m_message = w3m_https.stdout.read()
-    host_name = re_find_dns_name.search(w3m_message)
-    host_name = host_name and host_name.group(1) or None
-
-    return host_name
 
 
-class Ping(threading.Thread):
+
+def get_host(ip_address):
+    """
+    test ip is connected to port 443
+    """
+    try:
+        import requests
+    except ImportError as e:
+        print "Please 'pip install requests'"
+        return -1
+    try:
+        from requests.exceptions import SSLError, Timeout
+        headers=''
+        requests.get('https://' + ip_address, timeout=2)
+        return -2
+    except SSLError as e:
+        nameList = []
+        if "', '" in str(e):
+            nameList = str(e).split("', '")
+            nameList[0] = nameList[0].split("'")[-1]
+            nameList[-1] = nameList[-1].split("'")[0]
+            print ip_address
+            return nameList
+        elif 'match' in str(e):
+            tempList = str(e).split("'")
+            nameList.append(tempList[-2])
+            print ip_address
+            return nameList
+        return e
+    except Timeout:
+        # print ip_address + ' time out'
+        return -3
+    except Exception as e:
+        print ip_address + '\n' + str(e)
+        return -4
+
+
+
+
+
+
+
+def get_ip(ip_s):
+    """
+    read 3-tuplues, get host and filter to iter, it's some bugs because I don't know  thread well
+    """
+    print ip_s
+    for ip_tuple in ip_s:
+        for nu in range(ip_tuple[1], ip_tuple[2] + 1):
+            ip = ip_tuple[0] + '.' + str(nu)
+            get_thread = GetHost(ip)
+            tempList = get_thread.start()
+            if str(tempList).find('android.com') != -1:
+                yield {ip: 'android'}
+            if str(tempList).find('ggpht') != -1:
+                yield {ip: 'ggpht'}
+            if str(tempList).find('gstatic.com') != -1:
+                yield {ip: 'gstatic'}
+            if str(tempList).find('googleapis') != -1:
+                yield {ip: 'googleapis'}
+            if str(tempList).find('talk') != -1:
+                yield {ip: 'talk'}
+            if str(tempList).find('googleusercontent') != -1:
+                yield {ip: 'googleusercontent'}
+            if str(tempList).find('googlecode') != -1:
+                yield {ip: 'googlecode'}
+            if str(tempList).find('googlesource') != -1:
+                yield {ip: 'googlesource'}
+            if str(tempList).find('googlevideo') != -1:
+                yield {ip: 'googlevideo'}
+            if str(tempList).find('googlegroups') != -1:
+                yield {ip: 'googlegroups'}
+            if str(tempList).find('google') != -1:
+                yield {ip: 'google'}
+
+
+
+def dic_to_config(input_iter):
+    """
+    all iter change to config file
+    """
+    config = ConfigParser.RawConfigParser()
+    for m in input_iter:
+        keys = m.keys()[0]
+        values = m.values()[0]
+        from ConfigParser import DuplicateSectionError
+        try:
+            config.add_section(values)
+        except DuplicateSectionError:
+            pass
+        config.set(values, keys, values)
+    config.write(open('new_host_file', 'w'))
+
+
+
+def net_address(net_address_s):
+    """
+    "192.169.1.0/24" net address change to tuplues ("192.168.1", 0, 255)
+    """
+    ipList = []
+    def get_ip_number_list(m, ip_number):
+        n = 0
+        temp = 2 ** m
+        while True:
+            if m * n <= ip_number < m * (n + 1):
+                return [m * n, m * (n + 1)]
+            n += 1
+    for net in net_address_s:
+        netList = net.split('/')
+        if int(netList[1]) == 24:
+            yield (netList[0][:netList[0].rindex('.')], 0, 255)
+        elif 16 <= int(netList[1]) < 24:
+            m = 24 - int(netList[1])
+            ip_number = int(netList[0].split('.')[2])
+            ip_number_list = get_ip_number_list(m, ip_number)
+            for m in range(ip_number_list[0], ip_number_list[1]):
+                yield ('.'.join(netList[0].split('.')[:2] + [str(m)]), 0,255)
+
+
+class GetHost(threading.Thread):
+
     def __init__(self, ip_address):
         threading.Thread.__init__(self)
         self.ip_address = ip_address
 
     def run(self):
-        # ping IP Address, add 0% loss to ip_list
-        ping_cmd_command = ('ping', '-c 4', self.ip_address)
-        ping_cmd = subprocess.Popen(ping_cmd_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        ping_cmd.wait()
-
-        ping_cmd_echo = ping_cmd.stdout.read()
-        loss_percent = re_loss_percent.search(ping_cmd_echo)
-        loss_percent = int(loss_percent.group(1)) if loss_percent is not None else 200
-        avg_time = re_avg_time.search(ping_cmd_echo)
-        avg_time = avg_time and int(avg_time.group(1)) or 0
-
-        # Don't check high loss ip's host.
-        host_name = find_host(self.ip_address) if loss_percent < 80 else None
-
-        if host_name is not None:
-            # wait mutex and add to list
-            global lock
-            try:
-                lock.acquire()
-                ip_list.append((loss_percent, avg_time, self.ip_address, host_name))
-                #test enable. 测试用，我还是喜欢屏蔽掉，下边会输出排序好的。
-                print '%-6s%-4s%-16s%s' % (loss_percent, avg_time, self.ip_address, host_name)
-            except:
-                pass
-            finally:
-                lock.release()
-
-
-def list_ping(_set):
-    if ip_str is None or ip_str == '':
-        for _ in _set:
-            for i in range(_[1], _[2] + 1):
-                ping_thread = Ping('%s.%d' % (_[0], i))
-                ping_thread.start()
-    else:
-        ip = ip_str.split("|")
-        for _ in ip:
-            ping_thread = Ping(_)
-            ping_thread.start()
-
-    # once run threading.activeCount()=2 !!!maybe itself
-    print threading.activeCount() - init_threading_count, 'threading working...'
-    while threading.activeCount() > init_threading_count:
+        a = get_host(self.ip_address)
+        global lock
+        try:
+            lock.acquire()
+            return a
+        except:
+            pass
+        finally:
+            lock.release()
+    def stop(self):
         pass
-    
-    ip_list.sort()
-    # I want print by fast!!!  this doesn't work.
-    print '%-6s%-4s%-16s%s' % ('loss%', 'avg', 'IP Address', 'dNSName')
-    for ip in ip_list:
-        print '%-6s%-4s%-16s%s' % ip
 
-    print "you may love this."
-    _ip = []
-    for _ in ip_list:
-        if int(_[0]) == 0:
-            _ip.append(_[2])
-    print "|".join(_ip)
+def run_pro():
+    dic_to_config(get_ip(net_address(net_address_set)))
+
+
 
 
 if __name__ == '__main__':
     global lock
     lock = threading.Lock()
-    list_ping(ip_set)
+    run_pro()
+
